68646933d50b351662000e568003f001b283c923
d-k0de <d.ifechukwu@alustudent.com>
2025-02-21 18:30:32 +0200

    upate

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
+
 def top_ten(subreddit):
-    """If not a valid subreddit, print None"""
-    headers = {'User-Agent': 'python:top_ten:v1.0 (by /u/your_username)'}
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    params = {'limit': 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if response.status_code != 200:
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
-        return
-        
-    results = response.json().get('data', {}).get('children', [])
-    
-    if not results:
-        print(None)
-        return
-        
-    for post in results:
-        print(post.get('data', {}).get('title'))
