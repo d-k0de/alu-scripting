973a9664df47c36c202e61cb0f512bc4ea5bc049
d-k0de <d.ifechukwu@alustudent.com>
2025-02-21 18:00:16 +0200

    upate

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""DOC"""
+"""Contains the no. of subs function"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """DOC"""
+    """Returns the no. of subs function"""
     reddit_url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
 
